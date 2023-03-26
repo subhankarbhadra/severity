431942149c3e1ca139ffc3123c00ae086acaa58c
subhankarbhadra <128622980+subhankarbhadra@users.noreply.github.com>
2023-03-26 05:54:31 -0400

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 import csv
 import pickle
-import matplotlib.axes._subplots
+import matplotlib
 #import nltk
 #from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
