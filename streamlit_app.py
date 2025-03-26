8e5b26897a7faa23773fcb68dd2fa96b7b5c3d28
subhankarbhadra <128622980+subhankarbhadra@users.noreply.github.com>
2025-03-26 03:12:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,7 +162,7 @@ def main():
                  'and it is categorized as ', category, 'The severity score is higher than ', str(round(mperc, 1)), '% of malfunction events, ',
                  str(round(iperc, 1)), '% of injury events, and ', str(round(dperc, 1)), '% of death events from the MAUDE database.', unsafe_allow_html=True)
         
-        ax = pickle.load(open("score_plot.pickle", "rb"))
+        ax = pickle.load(open("scoreplot.pickle", "rb"))
         ax.axvline(x=prediction, color='blue')
         st.pyplot(ax.figure)
         
